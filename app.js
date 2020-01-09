29c460665b57f19fe6d8fccf67e86555b963c12d
Spencer Johnston <31662452+sjohnston1@users.noreply.github.com>
2020-01-09 12:59:56 -0600

    Updates for github pages

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var http = require("http");
-var bodyParser = require("body-parser");
-var app = express();
-
-app.use(express.static(__dirname + "/app"));
-app.use(bodyParser.urlencoded({ extended: false }));
-
-http.createServer(app).listen(3030);
